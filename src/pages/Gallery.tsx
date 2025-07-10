4797878964184cada236f1bae5f32c9b53884ec0
farza <farzanrichu610@gmail.com>
2025-07-10 17:57:26 +0530

    test commit ;new branch checkout

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -3,7 +3,7 @@ import ImageDatas from "../datas/ImageDatas";
 const Gallery = () => {
 	return <>
 	<div className="flex font-bold text-2xl flex-col justify-center h-screen bg-[983fd6] items-center">
-		<div className="flex flex-row justify-between items-center border-2 ">
+		<div className="flex flex-row justify-between items-center border-2 flex-wrap">
 			{ImageDatas.map((image, i) => (
 				<div key={i}>
 					<img src={image.src} alt={image.alt} />
