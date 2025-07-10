5da429eb2cf286aed856c7fca6fb24ea48c0e56b
farza <farzanrichu610@gmail.com>
2025-07-10 18:50:44 +0530

    fully implemented the masnory layout for Gallery.tsx

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,23 +1,27 @@
 import ImageDatas from "../datas/ImageDatas";
 
 const Gallery = () => {
-	return (
-		<div className="min-h-screen bg-[#983fd6] px-4 py-10">
-			<h2 className="text-center text-white text-3xl font-bold mb-8">Gallery</h2>
+    return (
+        <div className="min-h-screen bg-[#983fd6] px-4 py-10">
+            <h2 className="text-center text-white text-3xl font-bold mb-8">Gallery</h2>
 
-			<div className="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
-				{ImageDatas.map((image, i) => (
-					<div key={i} className="break-inside-avoid rounded overflow-hidden">
-						<img
-							src={image.src}
-							alt={image.alt}
-							className="w-full h-auto rounded-lg shadow-lg"
-						/>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+            <div className="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
+                {ImageDatas.map((image, i) => (
+                    <div key={i} className=" relative break-inside-avoid rounded overflow-hidden group  hover:scale-105 transition duration-300 ease-in-out">
+                        <img
+                            src={image.src}
+                            alt={image.alt}
+                            className="w-full h-auto rounded-lg shadow-lg  transition duration-300"/>
+                        <div className="absolute inset-0 flex items-end justify-center">
+                            <p className="text-white text-lg font-semibold mb-8  translate-y-full opacity-0 group-hover:translate-y-0 group-hover:opacity-100  transition duration-300 ease-in-out ">
+                                {image.text}
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Gallery;
