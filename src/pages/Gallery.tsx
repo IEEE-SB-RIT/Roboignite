ea0a4e4d634722c4c2fe1da1a309f64944c1ad15
farza <farzanrichu610@gmail.com>
2025-07-10 21:17:13 +0530

    update : remove bg color

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -2,8 +2,8 @@ import ImageDatas from "../datas/ImageDatas";
 
 const Gallery = () => {
     return (
-        <div className="min-h-screen bg-[#983fd6] px-4 py-10">
-            <h2 className="text-center text-white text-3xl font-bold mb-8">Gallery</h2>
+        <div className="min-h-screen  px-4 py-10">
+            <h2 className="text-center text-white  text-3xl font-bold mb-8">Gallery</h2>
 
             <div className="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
                 {ImageDatas.map((image, i) => (
