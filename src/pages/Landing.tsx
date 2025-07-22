388c720e35c5596b384aafc7709191c8e30ec2c2
Dhananjay R <88336915+dhjr@users.noreply.github.com>
2025-07-22 12:53:06 +0530

    optimized lcp element

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
 import { Typewriter } from "react-simple-typewriter";
-import { motion } from "motion/react";
 import RobotCanvas from "../components/RobotCanvas";
 import GradientBackground from "../components/gradientBg";
 // import SpaceBg from "../components/spaceBg";
@@ -48,17 +47,8 @@ const Landing = () => {
 					</h1>
 				</div>
 
-				<motion.img
-					initial={{ y: 10 }}
-					animate={{ y: 0 }}
-					exit={{ y: 10 }}
-					transition={{
-						duration: 2,
-						repeat: Infinity,
-						repeatType: "reverse",
-						damping: 10,
-					}}
-					className="bg-[url('/logoBg.webp')] inset-shadow-sm inset-shadow-indigo-400 bg-cover bg-no-repeat bg-center backdrop-blur-2xl mx-auto w-[18rem] md:w-[30rem] mt-10 shadow-[5px_15px_30px_0.1px_rgba(0,0,0,0.5)] shadow-violet-500/10 border-b-8 border-r-8 border-black/60  rounded-3xl py-5 px-10"
+				<img
+					className="bg-[url('/logoBg.webp')] inset-shadow-sm inset-shadow-indigo-400 bg-cover bg-no-repeat bg-center z-50 mx-auto w-[18rem] md:w-[30rem] mt-10 shadow-[5px_15px_30px_0.1px_rgba(0,0,0,0.5)] shadow-violet-500/20 border-b-8 border-r-8 border-black  rounded-3xl py-5 px-10"
 					src={import.meta.env.BASE_URL + "RoboIgnite.webp"}
 					alt="RoboIgnite Logo"
 				/>
