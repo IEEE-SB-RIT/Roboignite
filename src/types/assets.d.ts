ca6f38ea168e646a477699bab61e91090961278f
dhjr <dhananjayrjs1605@gmail.com>
2025-08-30 11:06:53 +0530

    Enhance SEO and accessibility by adding meta tags in index.html; create robots.txt and sitemap.xml for better indexing; clean up asset type declarations in assets.d.ts

--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -1,12 +1,26 @@
 // src/assets.d.ts
 
 declare module "*.png" {
-	const value: string;
-	export default value;
+  const value: string;
+  export default value;
 }
-
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.svg";
-declare module "*.gif";
-declare module "*.webp";
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
+}
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+declare module "*.gif" {
+  const value: string;
+  export default value;
+}
+declare module "*.webp" {
+  const value: string;
+  export default value;
+}
