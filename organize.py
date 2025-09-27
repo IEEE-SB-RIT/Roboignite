ad65971e728a89d57bfbeb60c9d81014dd733914
farza <farzanrichu610@gmail.com>
2025-09-27 16:43:30 +0530

    Made a python script to convert the images from jpg to webp

--- organize.py
+++ organize.py
@@ -1,1 +1,22 @@
 import os
+from PIL import Image
+source_path="C:\\Users\\farza\\WebstormProjects\\Roboignite\\public\\Organize"
+dest_path="C:\\Users\\farza\\WebstormProjects\\Roboignite\\public\\roboGallery"
+
+os.makedirs(dest_path, exist_ok=True)
+
+extension="jpg"
+files = [f for f in os.listdir(source_path) if f.lower().endswith(extension.lower())]
+
+
+for idx,file in enumerate(files,start=1):
+    img_path = os.path.join(source_path, file)
+    new_file_name = f"Day1_{idx:02d}.webp"
+    save_path = os.path.join(dest_path, new_file_name)
+
+    with Image.open(img_path) as img:
+        img.save(save_path, "WEBP", quality=90)
+
+    os.remove(img_path)
+
+    print(f"Image {file} has been saved to destination as {new_file_name}")
